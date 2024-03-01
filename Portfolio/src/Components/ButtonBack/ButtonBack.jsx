8565453e25e1693fbe8f626a94f64013598a8eab
negrura14 <talleresmatador114@gmail.com>
2024-03-01 12:49:08 -0300

    cambios

--- Portfolio/src/Components/ButtonBack/ButtonBack.jsx
+++ Portfolio/src/Components/ButtonBack/ButtonBack.jsx
@@ -3,28 +3,27 @@ import { Link, animateScroll as scroll } from "react-scroll";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 
 const ButtonBack = () => {
-    const [show, setShow] = useState(false);
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            return window.scrollY > 600 ? setShow(true) : setShow(false);
-        })
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      return window.scrollY > 600 ? setShow(true) : setShow(false);
     });
+  });
 
-    const scrollToTop = () => {
-        scroll.scrollToTop();
-    };
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
 
-    return (
-        show && (
-            <button
-            onClick={() => scrollToTop()}
-            className="bg-accent hover:bg-accent-hover tect-white w-12 h-12 rounded-full fixed right-24 bottom-24 cursor-pointer flex justify-center items-center transition-all"
-            >
-                <ChevronUpIcon className="w-6 h-6"/>
-
-            </button>
-        )
+  return (
+    show && (
+      <button
+        onClick={() => scrollToTop()}
+        className="bg-accent hover:bg-accent-hover tect-white w-12 h-12 rounded-full fixed right-24 bottom-24 cursor-pointer flex justify-center items-center transition-all"
+      >
+        <ChevronUpIcon className="w-6 h-6" />
+      </button>
     )
-}
+  );
+};
 
 export default ButtonBack;
